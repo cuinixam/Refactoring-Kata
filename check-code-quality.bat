1c71ed361ab194311f6a2587bc719e8bd72fc257
Eggert, Matthias (SD-RM) <Matthias.Eggert@marquardt.com>
2022-11-24 18:03:55 +0100

    add functionality to check script

--- check-code-quality.bat
+++ check-code-quality.bat
@@ -1,3 +1,15 @@
 @echo off
 
+echo ###### static code analysis ######
 python -m pipenv run pylama python/gilded_rose.py
+set quality=%ERRORLEVEL%
+
+echo .
+echo ###### unit test ######
+python -m pipenv run pytest
+set test=%ERRORLEVEL%
+
+echo .
+echo ###### results ######
+echo static code analysis result is: %ERRORLEVEL% 
+echo unit test result is: %ERRORLEVEL% 

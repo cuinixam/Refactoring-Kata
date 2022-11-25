e3d5816ce003fa083688ea0d099d58fe28d01b55
Guenther, Karsten (SD-RM) <Karsten.Guenther@marquardt.com>
2022-11-25 09:54:32 +0100

    Test sell_in decrease

--- python/test_gilded_rose.py
+++ python/test_gilded_rose.py
@@ -5,11 +5,13 @@ from gilded_rose import Item, GildedRose
 
 
 class GildedRoseTest(unittest.TestCase):
-    def test_foo(self):
-        items = [Item("foo", 0, 0)]
+    def test_item_trivial(self):
+        """Tested: Req-001, Req-003"""
+        items = [Item("trivial", 0, 0)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
-        self.assertEquals("foo", items[0].name)
+        self.assertEquals("trivial", items[0].name)
+        self.assertEquals(-1, items[0].sell_in)
 
         
 if __name__ == '__main__':

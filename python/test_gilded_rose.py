b6db7d15f309750123f7f0a2c391828c5bbd9be9
Guenther, Karsten (SD-RM) <Karsten.Guenther@marquardt.com>
2022-11-25 11:10:20 +0100

    Test Req-009

--- python/test_gilded_rose.py
+++ python/test_gilded_rose.py
@@ -66,14 +66,13 @@ class GildedRoseTest(unittest.TestCase):
         # Assertions
         self.assertEquals(sell_in__before - 1, items[0].sell_in)
         self.assertEquals(quality__before + 1, items[0].quality)
-        
+
         # call iut
         gilded_rose.update_quality()
 
         # Assertions
         self.assertEquals(sell_in__before - 2, items[0].sell_in)
         self.assertEquals(quality__before + 3, items[0].quality)
-
 
     def test_item_max_quality(self):
         """Tested: Req-007"""
@@ -88,57 +87,72 @@ class GildedRoseTest(unittest.TestCase):
         gilded_rose.update_quality()
 
         # Assert expectations
-        self.assertEquals(quality__before + 1 , items[0].quality)
-        
+        self.assertEquals(quality__before + 1, items[0].quality)
+
         # Call item-under-test
         gilded_rose.update_quality()
 
         # Assert expectations
-        self.assertEquals(quality__before + 1 , items[0].quality)
-        
-        
+        self.assertEquals(quality__before + 1, items[0].quality)
+
     def test_xx(self):
         """Tested: Req-008"""
 
         # Preconditions
-        name            = "Sulfuras, Hand of Ragnaros"
+        name = "Sulfuras, Hand of Ragnaros"
         sell_in__before = 1
         quality__before = 1
-        
+
         items = [Item(name, sell_in__before, quality__before)]
         gilded_rose = GildedRose(items)
-        
+
         # Call item-under-test
         gilded_rose.update_quality()
-        
+
         # Assert expectations
-        self.assertEquals(quality__before , items[0].quality)
-        self.assertEquals(sell_in__before , items[0].sell_in)
+        self.assertEquals(quality__before, items[0].quality)
+        self.assertEquals(sell_in__before, items[0].sell_in)
 
     def test_backstagePass(self):
         """Tested: Req-009"""
 
         # Preconditions
-        name            = "Backstage passes to a TAFKAL80ETC concert"
+        name = "Backstage passes to a TAFKAL80ETC concert"
         sell_in__before = 11
         quality__before = 1
-        
-        items = [Item(name, sell_in__before, quality__before)]
-        gilded_rose = GildedRose(items)
-        
-        # Call item-under-test
-        gilded_rose.update_quality()
-        
-        # Assert expectations
-        self.assertEquals(quality__before + 1 , items[0].quality)
-        self.assertEquals(sell_in__before - 1 , items[0].sell_in)
+
+        item = Item(name, sell_in__before, quality__before)
+        gilded_rose = GildedRose([item])
 
         # Call item-under-test
         gilded_rose.update_quality()
-        
+
         # Assert expectations
-        self.assertEquals(quality__before + 3 , items[0].quality)
-        self.assertEquals(sell_in__before - 2 , items[0].sell_in)
+        self.assertEquals(quality__before + 1, item.quality)
+        self.assertEquals(sell_in__before - 1, item.sell_in)
+
+        # Call item-under-test
+        gilded_rose.update_quality()
+
+        # Assert expectations
+        self.assertEquals(quality__before + 3, item.quality)
+        self.assertEquals(sell_in__before - 2, item.sell_in)
+
+        # Call item-under-test
+        for _ in range(4):
+            gilded_rose.update_quality()
+
+        # Assert expectations
+        self.assertEquals(12, item.quality)
+        self.assertEquals(5, item.sell_in)
+
+        # Call item-under-test
+        gilded_rose.update_quality()
+
+        # Assert expectations
+        self.assertEquals(15, item.quality)
+        self.assertEquals(4, item.sell_in)
+
 
 if __name__ == '__main__':
     unittest.main()

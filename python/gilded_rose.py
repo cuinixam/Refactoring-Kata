5729fb80c048af3344ab17c0030ab67e8ff2a5e7
Maxiniuc, Alexandru (SD-RM) <Alexandru.Maxiniuc@marquardt.com>
2023-01-12 16:16:34 +0100

    fix a lot of docstrings and some other stuff

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -1,58 +1,74 @@
 """Implements the GildedRose requirements. @see requirements.md."""
 
 from abc import ABC, abstractmethod
-from typing import Dict
+from dataclasses import dataclass
+from typing import Dict, List
 
 
+@dataclass
 class Item:
-    def __init__(self, name, sell_in, quality):
-        self.name = name
-        self.sell_in = sell_in
-        self.quality = quality
+    """Holds data for an item."""
+
+    name: str
+    sell_in: int
+    quality: int
 
     def __repr__(self):
-        return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
+        """Make a custom string representation."""
+        return f"{self.name}, {self.sell_in}, {self.quality}"
+
 
 class ItemUpdater(ABC):
-    
+    """Item updater base class."""
+
     def __init__(self, item: Item) -> None:
+        """I have no idea why anyone would document the __init__ method.
+
+        Args:
+            item (Item): _description_
+        """
         self.item = item
-    
+
     @abstractmethod
-    def update_quality(self, item: Item):
-        """Updates the Item quality"""
-        
+    def update_quality(self):
+        """Updates the Item quality."""
+
     def increase_quality(self, amount: int = 1):
-        self.item.quality += amount
-        if self.item.quality > 50:
-            self.item.quality = 50
-            
+        """Req 007."""
+        self.item.quality = min(self.item.quality + amount, 50)
+
     def decrease_quality(self, amount: int = 1):
-        self.item.quality -= amount
-        if self.item.quality < 0:
-            self.item.quality = 0
-        
+        """Req 005."""
+        self.item.quality = max(self.item.quality - amount, 0)
+
     def decrease_sell_in(self):
+        """Decrease sell in."""
         if self.item.quality > -1:
             self.item.sell_in -= 1
-        
+
     def is_expired(self) -> bool:
         return self.item.sell_in < 0
-        
+
 
 class TrivialItemUpdater(ItemUpdater):
+    """Updater for trivial items."""
+
     def update_quality(self):
         self.decrease_sell_in()
         self.decrease_quality(2 if self.is_expired() else 1)
 
 
 class OldCheeseItemUpdater(ItemUpdater):
+    """Updater for old cheese."""
+
     def update_quality(self):
         self.decrease_sell_in()
         self.increase_quality(2 if self.is_expired() else 1)
 
 
 class TicketItemUpdater(ItemUpdater):
+    """Updater for tickets."""
+
     def update_quality(self):
         self.decrease_sell_in()
         if self.is_expired():
@@ -64,35 +80,44 @@ class TicketItemUpdater(ItemUpdater):
             if self.item.sell_in < 6:
                 self.increase_quality()
 
+
 class LegendaryItemUpdater(ItemUpdater):
+    """Updater for legendary items."""
+
     def update_quality(self):
         pass
 
 
 class GildedRose:
     """Implements the GildedRose requirements. @see requirements.md."""
-    
+
     item_updaters_mapping: Dict[str, ItemUpdater] = {
         "Sulfuras, Hand of Ragnaros": LegendaryItemUpdater,
         "Backstage passes to a TAFKAL80ETC concert": TicketItemUpdater,
         "Aged Brie": OldCheeseItemUpdater
     }
 
-    def __init__(self, items):
+    def __init__(self, items: List[Item]):
+        """I have no idea why anyone will document the __init__ method.
+
+        Args:
+            items (_type_): _description_
+        """
         self.items = items
 
     def update_quality(self):
-        """called once at the end of each day"""
+        """Called once at the end of each day."""
         for item in self.items:
             if self.item_updaters_mapping.get(item.name, None) is not None:
                 self.item_updaters_mapping.get(item.name)(item).update_quality()
-            else: # all other items are trivial
+            else:  # all other items are trivial
                 TrivialItemUpdater(item).update_quality()
 
 
 def main():
+    """Why would anyone document the main method."""
     my_gilde = GildedRose([])
-    while(1):
+    while True:
         my_gilde.update_quality()
 
 

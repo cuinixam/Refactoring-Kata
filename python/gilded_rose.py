cd210c95ec25c594dc70295649f656f7fda65c44
Maxiniuc, Alexandru (SD-RM) <Alexandru.Maxiniuc@marquardt.com>
2023-01-12 15:18:00 +0100

    refactor the updater

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -1,50 +1,7 @@
 """Implements the GildedRose requirements. @see requirements.md."""
 
-
-class GildedRose:
-
-    """Implements the GildedRose requirements. @see requirements.md."""
-
-    def __init__(self, items):
-        self.items = items
-
-    def update_quality(self):
-        """called once at the end of each day"""
-        for item in self.items:
-            if item.name != "Sulfuras, Hand of Ragnaros": # Req-008
-                self.update_item_quality(item)
-                self.update_item_sell_in(item)
-                self.update_expired_item_quality(item)
-
-    def update_expired_item_quality(self, item):
-        if item.sell_in < 0:
-            if item.name != "Aged Brie":
-                if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                    if item.quality > 0:
-                        item.quality = item.quality - 1
-                else:
-                    item.quality = item.quality - item.quality
-            else:
-                if item.quality < 50:
-                    item.quality = item.quality + 1
-
-    def update_item_sell_in(self, item):
-        item.sell_in = item.sell_in - 1
-
-    def update_item_quality(self, item):
-        if item.quality > 0 and item.quality < 50 : # Req-005 and Req-007
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                item.quality = item.quality - 1
-            else:
-                item.quality = item.quality + 1
-                if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                    if item.sell_in < 11:
-                        item.quality = item.quality + 1
-                    if item.sell_in < 6:
-                        item.quality = item.quality + 1
-                        
-    def dummy(self):
-        print('test')
+from abc import ABC, abstractmethod
+from typing import Dict
 
 
 class Item:
@@ -56,11 +13,88 @@ class Item:
     def __repr__(self):
         return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
 
+class ItemUpdater(ABC):
+    
+    def __init__(self, item: Item) -> None:
+        self.item = item
+    
+    @abstractmethod
+    def update_quality(self, item: Item):
+        """Updates the Item quality"""
+        
+    def increase_quality(self, amount: int = 1):
+        self.item.quality += amount
+        if self.item.quality > 50:
+            self.item.quality = 50
+            
+    def decrease_quality(self, amount: int = 1):
+        self.item.quality -= amount
+        if self.item.quality < 0:
+            self.item.quality = 0
+        
+    def decrease_sell_in(self):
+        if self.item.quality > -1:
+            self.item.sell_in -= 1
+        
+    def is_expired(self) -> bool:
+        return self.item.sell_in < 0
+        
+
+class TrivialItemUpdater(ItemUpdater):
+    def update_quality(self):
+        self.decrease_sell_in()
+        self.decrease_quality(2 if self.is_expired() else 1)
+
+
+class OldCheeseItemUpdater(ItemUpdater):
+    def update_quality(self):
+        self.decrease_sell_in()
+        self.increase_quality(2 if self.is_expired() else 1)
+
+
+class TicketItemUpdater(ItemUpdater):
+    def update_quality(self):
+        self.decrease_sell_in()
+        if self.is_expired():
+            self.item.quality = 0
+        else:
+            self.increase_quality()
+            if self.item.sell_in < 11:
+                self.increase_quality()
+            if self.item.sell_in < 6:
+                self.increase_quality()
+
+class LegendaryItemUpdater(ItemUpdater):
+    def update_quality(self):
+        pass
+
+
+class GildedRose:
+    """Implements the GildedRose requirements. @see requirements.md."""
+    
+    item_updaters_mapping: Dict[str, ItemUpdater] = {
+        "Sulfuras, Hand of Ragnaros": LegendaryItemUpdater,
+        "Backstage passes to a TAFKAL80ETC concert": TicketItemUpdater,
+        "Aged Brie": OldCheeseItemUpdater
+    }
+
+    def __init__(self, items):
+        self.items = items
+
+    def update_quality(self):
+        """called once at the end of each day"""
+        for item in self.items:
+            if self.item_updaters_mapping.get(item.name, None) is not None:
+                self.item_updaters_mapping.get(item.name)(item).update_quality()
+            else: # all other items are trivial
+                TrivialItemUpdater(item).update_quality()
+
 
 def main():
     my_gilde = GildedRose([])
     while(1):
         my_gilde.update_quality()
+
 
 if __name__ == "main":
     main()

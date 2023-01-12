543997ec59a47ab458739c6f9188a3ed26577f3e
Maxiniuc, Alexandru (SD-RM) <Alexandru.Maxiniuc@marquardt.com>
2023-01-12 11:09:47 +0100

    optimize Sulfuras updates

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -11,17 +11,17 @@ class GildedRose:
     def update_quality(self):
         """called once at the end of each day"""
         for item in self.items:
-            self.update_item_quality(item)
-            self.update_item_sell_in(item)
-            self.update_expired_item_quality(item)
+            if item.name != "Sulfuras, Hand of Ragnaros": # Req-008
+                self.update_item_quality(item)
+                self.update_item_sell_in(item)
+                self.update_expired_item_quality(item)
 
     def update_expired_item_quality(self, item):
         if item.sell_in < 0:
             if item.name != "Aged Brie":
                 if item.name != "Backstage passes to a TAFKAL80ETC concert":
                     if item.quality > 0:
-                        if item.name != "Sulfuras, Hand of Ragnaros":
-                            item.quality = item.quality - 1
+                        item.quality = item.quality - 1
                 else:
                     item.quality = item.quality - item.quality
             else:
@@ -29,14 +29,12 @@ class GildedRose:
                     item.quality = item.quality + 1
 
     def update_item_sell_in(self, item):
-        if item.name != "Sulfuras, Hand of Ragnaros":
-            item.sell_in = item.sell_in - 1
+        item.sell_in = item.sell_in - 1
 
     def update_item_quality(self, item):
         if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-            if item.quality > 0:
-                if item.name != "Sulfuras, Hand of Ragnaros":
-                    item.quality = item.quality - 1
+            if item.quality > 0: # Req-005
+                item.quality = item.quality - 1
         else:
             if item.quality < 50:
                 item.quality = item.quality + 1
